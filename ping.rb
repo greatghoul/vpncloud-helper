628480ee6bbf5b320ad5b86cc091eb98f4d8bb1e
bydmm <521194743@qq.com>
2013-07-22 15:14:56 +0800

    color

--- ping.rb
+++ ping.rb
@@ -1,4 +1,5 @@
 require 'net/ping'
+require 'colorize'
 
 def ping url
   sum = 0
@@ -10,15 +11,24 @@ def ping url
       cost = Time.now - from
       sum = sum + cost * 1000
     else
+      if bugs == 0
+        puts "ping #{url}: #{pt.exception}".colorize( :yellow )
+      end
       bugs = bugs + 1
-      puts "ping #{url}: #{pt.exception}"
     end
   end
   sum = (sum / 5).round
-  puts "ping #{url} : #{sum}ms, failed: #{bugs}.times"
+  outputs = "ping #{url} : #{sum}ms, failed: #{bugs}.times"
+  if bugs == 0
+    puts outputs.colorize( :green )
+  elsif bugs > 1 and bugs < 5
+    puts outputs.colorize( :yellow )
+  elsif bugs == 5
+    puts outputs.colorize( :red )
+  end
 end
 
-ping "baidu.com"
+ping "www.baidu.com"
 ping 'us1.vpncloud.me'
 ping 'us2.vpncloud.me'
 ping 'us3.vpncloud.me'
